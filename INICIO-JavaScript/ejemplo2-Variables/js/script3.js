63adce88cfba634749305f8edd12701ca0f737f4
@ndragogo´s formacion <78384046+ANTONIOa7o@users.noreply.github.com>
2023-04-21 14:37:15 +0200

    Add files via upload

--- INICIO-JavaScript/ejemplo2-Variables/js/script3.js
+++ INICIO-JavaScript/ejemplo2-Variables/js/script3.js
@@ -1,12 +1,47 @@
 
 //1º declaro variable
 var nombre;
+var apellidos;
+var edad;
 //2º Asignar valor
- /* nombre ='Toño'   */
+/* nombre ='Toño'   */
 //3º Asignarlo a través de un metodo
- nombre=prompt('Introduce tu nombre');  
- /*
- En ocasiones el propio sistema operativo del usurio contribuye a la gestion y el buen
- funcionamiento de los recurso.
- window.prompt()
- */
+nombre = window.prompt('Introduce tu nombre');
+apellidos = prompt('Introduce tus apellidos');
+edad = prompt('Introduce tu edad');
+/*
+En ocasiones el propio sistema operativo del usurio contribuye a la gestion y el buen
+funcionamiento de los recurso.
+window.prompt()
+*/
+
+/*1º*/
+document.write(
+    '<h2>Acceso confirmado al Curso Programacion lenguajes de guión; </h2>' +
+   '<p>'+ nombre + ' ' +
+    apellidos +
+    ', con una edad de:  ' +
+    edad + ' años'+
+    '</p>'
+    );
+/*2º*/
+console.log(
+    'Acceso permitido a' + '\n'+
+    nombre + 
+    ' ' + 
+    apellidos + 
+    '\n'+
+    'con edad de: ' +
+    edad + 'años'  
+);
+
+/*3º*/
+swal(
+    {
+        title: "ACCESO!",
+        text: "Autorizado",
+        icon: "success",
+        button: "Continuar",
+    }
+);
+
---
+++ INICIO-JavaScript/ejemplo2-Variables/js/script6.js
@@ -0,0 +1,16 @@
+//1 declaracion variables eje
+var dato1 = parseInt(prompt('Introduce 1º numero 🚀'));
+var dato2 = parseInt(prompt('introduce tu 2º número 🎯'));
+//2 variable secundario
+var suma = (dato1 + dato2);
+var resta = (dato1 - dato2);
+var multiplicacion = (dato1 * dato2);
+var division = (dato1 / dato2);
+//3 Mostra las variables
+swal('los valores seleccionados han sido: ' + dato1 + ' &' + dato2 + '\n' +
+    'la suma es: ' + suma + '\n' +
+    'la division es: ' + division + '\n' +
+    'la multiplicacion es: ' + multiplicacion +'\n' +
+    'la resta es: ' + resta + '\n' +
+    'la division es: ' + division
+);
